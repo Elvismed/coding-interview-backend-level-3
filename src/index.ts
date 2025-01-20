ed7a6cae52b83dc8ba9b3f8c09bfcfdf9de55b17
elvismed <elvisrmdiaz@gmail.com>
2025-01-19 21:00:41 -0400

    adding postman collection

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import cors from "cors";
 import itemRoutes from "./routes/item.routes";
 const AutoIncrement = require("mongoose-sequence")(mongoose);
 const app = express();
-
+// using express instead of hapi as server in this case
 const PORT: string | number = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use(itemRoutes);
-
+// setting uri to the mongo db uri or to the default one
 const uri: string =
   process.env.MONGO_DB_URI || "mongodb://localhost:27017/item_db";
 mongoose
